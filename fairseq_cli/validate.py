11d3f9442fc3e3396b44924fb0ffa215b28a0f52
ForTheNext <35482123+AnonymousRes@users.noreply.github.com>
2026-06-12 14:05:02 +0800

    Update validate.py

--- fairseq_cli/validate.py
+++ fairseq_cli/validate.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import sys
-# sys.path.insert(0, "/workspace/Bi-RAM")
+sys.path.insert(0, "/workspace/Bi-RAM")
 from itertools import chain
 import logging
 
